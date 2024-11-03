a4afd7e7b3ee6b35ea8740f3cc6dda9c25431f81
sholex111 <sholex111@gmail.com>
2024-11-03 22:10:19 +0000

    backend setup

--- backend.tf
+++ backend.tf
@@ -1,9 +1,9 @@
 
 #backend.tf
 
-#terraform {
-#  backend "gcs" {
-#    bucket = "shoxappbucket1"  # Use the static name of the state bucket
-#    prefix = "terraform/state"
-#  }
-#}
+terraform {
+  backend "gcs" {
+    bucket = "shoxappbucket1"  # Use the static name of the state bucket
+    prefix = "terraform/state"
+  }
+}
